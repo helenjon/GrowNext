1460bf44a89932f23daf303944bd90a02bc37f20
helenjon <helena.mokina@gmail.com>
2020-02-19 16:56:48 +0200

    WebDriverManager added

--- src/test/java/webdrivers/WebDriverr.java
+++ src/test/java/webdrivers/WebDriverr.java
@@ -1,8 +1,11 @@
 package webdrivers;
 
+import io.github.bonigarcia.wdm.ChromeDriverManager;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -10,10 +13,14 @@ import java.io.IOException;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
+import static io.github.bonigarcia.wdm.DriverManagerType.CHROME;
+import static io.github.bonigarcia.wdm.DriverManagerType.FIREFOX;
+
 public class WebDriverr {
 
     public static Properties properties;
-    public static WebDriver driver=null;
+    public static WebDriver driver;
+    public static WebDriverWait wait;
 
     public WebDriverr(){
         try {
@@ -28,28 +35,38 @@ public class WebDriverr {
         }
     }
 
-    public static WebDriver initialization(){
+    public static WebDriver initialization()  {
+        WebDriverManager.firefoxdriver().setup();
         new WebDriverr();
         String driverName = properties.getProperty("browser");
         if(driver == null)
         {
             if(driverName.equalsIgnoreCase("chrome"))
             {
+                WebDriverManager.getInstance(CHROME).setup();
+                System.out.println(WebDriverManager.chromedriver());
                 driver=new ChromeDriver();
+
             }
-            else if(driverName.equalsIgnoreCase("Firefox"))
+            else if(driverName.equalsIgnoreCase("firefox"))
             {
+                WebDriverManager.getInstance(FIREFOX).setup();
+                System.out.println(WebDriverManager.firefoxdriver());
                 driver=new FirefoxDriver();
+
+
             }
+
         }
         //Perform Basic Operations
         driver.manage().deleteAllCookies();
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+
+        driver.manage().timeouts().implicitlyWait(Integer.parseInt(properties.getProperty("implicitlyWait")), TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(Integer.parseInt(properties.getProperty("pageLoadTimeout")), TimeUnit.SECONDS);
+        wait=new WebDriverWait(driver, Integer.parseInt(properties.getProperty("wait")));
 
         return driver;
-
     }
 
 }
